b9ffd8d67c36353148a97959a82773ad3e9ee62a
detronetdip <ayondip2001@gmail.com>
2022-04-02 21:01:31 +0530

    added

--- deliveryBoy/require/nav.php
+++ deliveryBoy/require/nav.php
@@ -81,6 +81,6 @@
           </ul>
         </div>
         <div class="copyright">
-          <p>Developed by Ask in city, Copyright@2021, All rights reserverd</p>
+          <p>Developed by Ayondip Jana</p>
         </div>
       </div>
